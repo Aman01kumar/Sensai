0f337de929dcec062cb3c873748d6b12bcbeda6c
Your Name <you@example.com>
2025-06-27 16:42:36 +0530

    Modifications

--- actions/user.js
+++ actions/user.js
@@ -3,6 +3,7 @@
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { _success } from "zod/v4/core";
+import { generateAIInsights } from "./dashboard";
 
 
 export async function updateUser(data) {
@@ -28,19 +29,15 @@ export async function updateUser(data) {
 
         // If industry doesn't exist, create it with default values
         if (!industryInsight) {
-          industryInsight = await tx.industryInsight.create ({
-            data: {
-              industry: data.industry,
-              salaryRanges: [],
-              growthRate: 0,
-              demandLevel: "MEDIUM",
-              topSkills: [],
-              marketOutlook: "NEUTRAL", 
-              keyTrends: [], 
-              recommendedSkills: [],
-              nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 1 week from now
-            },
-          });
+          const insights = await generateAIInsights(data.industry);
+          
+              industryInsight = await db.industryInsight.create({
+                data: {
+                  industry: data.industry,
+                  ...insights,
+                  nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+                },
+              });
         }
 
         // Now update the user
